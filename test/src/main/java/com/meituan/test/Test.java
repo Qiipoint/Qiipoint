8724d1d97b1a47e92274cbe7551d42ba0864fba0
Qiipoint <hbnuyq@163.com>
2021-11-17 15:31:48 +0800

    tijiao

---
+++ test/src/main/java/com/meituan/test/Test.java
@@ -0,0 +1,14 @@
+package com.meituan.test;
+
+import javax.crypto.Cipher;
+
+/**
+ * @Author: Qiipoint
+ * @CreateTime: 11-15/2021 22:35
+ * @Description:
+ */
+public class Test {
+    public static void main(String[] args) {
+        String a = "abc";
+    }
+}
